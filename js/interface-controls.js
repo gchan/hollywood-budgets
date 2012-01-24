d928f0405f9b12521ff3c3ae4a5595943c3f2cbd
Gordon Chan <developer.gordon@gmail.com>
2012-01-24 22:57:26 +1300

    slider updates data while filtering. set slider steping to 50

--- js/interface-controls.js
+++ js/interface-controls.js
@@ -1,25 +1,56 @@
-var updateSliderRange = function(min, max){
-    min = min ? min : 0;
-    max = max ? max : 0;
+function updateSliderRange(min, max){
+    min = min ? Math.round(min) : 0;
+    max = max ? Math.round(max) : 0;
     
     $( "#slider-range" ).slider({
-        min: Math.round(min),
-        max: Math.round(max),
+        min: min,
+        max: max,
         values: [min, max],
-    });       
-    $( "#amount" ).text( "Worldwide Gross: $" + $( "#slider-range" ).slider( "values", 0 ) + "m - $" + $( "#slider-range" ).slider( "values", 1 ) + "m" );
+    });
+ 
+    $( "#amount" ).text( "Worldwide Gross: $" + min + "m - $" + max + "m" );
 };
     
+function filterIfSteady(min, max){
+    if (min == $( "#slider-range" ).slider( "values", 0 ) && max == $( "#slider-range" ).slider( "values", 1 ))
+        renderSelection();
+};
+
+function renderSelection(){
+    var stories = [];
+    $(".btn.story").each(function(i, e){
+        var btn = $(e);
+        if(btn.hasClass("primary"))
+            stories.push(btn.text().toLowerCase());
+    });
+    
+    var years = [];
+    $(".btn.year").each(function(i, e){
+        var btn = $(e);
+        if(btn.hasClass("primary"))
+            years.push(parseInt(btn.text()));
+    });
+    
+    var grossRange = [];
+    grossRange.push($( "#slider-range" ).slider( "values", 0 ));
+    grossRange.push($( "#slider-range" ).slider( "values", 1 ));
+    
+    showFiltered(years, stories, grossRange);
+}
+    
 $(document).ready(function() {
+
     $( "#slider-range" ).slider({
         animate: true,
         range: true,
         min: 0,
-        //step: 50,
+        step: 50,
         max: 1000,
         values: [0, 1000],
         slide: function( event, ui ) {
             $( "#amount" ).text( "Worldwide Gross: $" + ui.values[ 0 ] + "m - $" + ui.values[ 1 ] + "m");
+            
+            setTimeout("filterIfSteady(" + ui.values[ 0 ] + "," + ui.values[ 1 ] + ")" , 200);
         },
         stop: function(){
             renderSelection();
@@ -39,28 +70,6 @@ $(document).ready(function() {
         
         renderSelection();
     });
-    
-    function renderSelection(){
-        var stories = [];
-        $(".btn.story").each(function(i, e){
-            var btn = $(e);
-            if(btn.hasClass("primary"))
-                stories.push(btn.text().toLowerCase());
-        });
-        
-        var years = [];
-        $(".btn.year").each(function(i, e){
-            var btn = $(e);
-            if(btn.hasClass("primary"))
-                years.push(parseInt(btn.text()));
-        });
-        
-        var grossRange = [];
-        grossRange.push($( "#slider-range" ).slider( "values", 0 ));
-        grossRange.push($( "#slider-range" ).slider( "values", 1 ));
-        
-        showFiltered(years, stories, grossRange);
-    }
     
     $(".btn.story").hover(
         function(e){
